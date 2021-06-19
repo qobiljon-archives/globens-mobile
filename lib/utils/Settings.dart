1eb5c904c3d7e486cee07a2d825a867e9c585cf1
Qobiljon <qobiljon.toshnazarov@gmail.com>
2021-06-19 19:32:00 +0900

    server ip changed

--- lib/utils/Settings.dart
+++ lib/utils/Settings.dart
@@ -1,7 +1,7 @@
 import 'package:globens_flutter_client/generated_protos/gb_service.pbgrpc.dart';
 import 'package:grpc/grpc.dart';
 
-const String GRPC_HOST = '54.180.83.68';
+const String GRPC_HOST = 'globens.ddns.net';
 const int GRPC_PORT = 50051;
 
 ClientChannel _channel;
